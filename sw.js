54d5fba42d58f28789cdcabd4fdce316b916a932
SuperdaD <danilomagbual@gmail.com>
2026-06-12 10:47:56 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='accaza-v1';
+const CACHE='accaza-v2';
 const ASSETS=['/','/index.html','/favicon_192x192.png','/favicon_512x512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
