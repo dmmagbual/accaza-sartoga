c4e0b5b45a0396f7cf095fd3cf1303b36de33ccd
Danilo Magbual <danilomagbual@gmail.com>
2026-08-29 21:57:08 +1000

    fix(syntax): remove malformed parentheses and fix e2e alt tags

--- functions/src/core/firebase.js
+++ functions/src/core/firebase.js
@@ -1,1 +1,8 @@
-﻿("use strict"; const { initializeApp, getApps } = require("firebase-admin/app"); const { getAuth } = require("firebase-admin/auth"); const { getDatabase } = require("firebase-admin/database"); const { getMessaging } = require("firebase-admin/messaging"); const { getStorage } = require("firebase-admin/storage"); if (!getApps().length) initializeApp(); module.exports = { getAdminAuth: getAuth, getDatabase, getMessaging, getStorage };)
+﻿"use strict";
+const { initializeApp, getApps } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+const { getDatabase } = require("firebase-admin/database");
+const { getMessaging } = require("firebase-admin/messaging");
+const { getStorage } = require("firebase-admin/storage");
+if (!getApps().length) initializeApp();
+module.exports = { getAdminAuth: getAuth, getDatabase, getMessaging, getStorage };
